c0bc07e043608e9040afebbb7c52eddccfa65aee
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2022-09-02 15:49:48 +0200

    Working draft on frequency detection

--- src_mcu/src/main.cpp
+++ src_mcu/src/main.cpp
@@ -21,61 +21,140 @@
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <avdweb_Switch.h>
 
-#define PIN_BUTTON_A 9
-#define PIN_BUTTON_B 6
-#define PIN_BUTTON_C 5
-#define PIN_TACHO 10
+// Tacho settings
+const uint8_t PIN_TACHO = 10;
+const uint8_t N_SLITS_ON_DISK = 50;
+
+// OLED display
+const uint8_t PIN_BUTTON_A = 9;
+const uint8_t PIN_BUTTON_B = 6;
+const uint8_t PIN_BUTTON_C = 5;
+Switch button_A = Switch(PIN_BUTTON_A, INPUT_PULLUP);
+Switch button_B = Switch(PIN_BUTTON_B, INPUT_PULLUP);
+Switch button_C = Switch(PIN_BUTTON_C, INPUT_PULLUP);
 
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
+const uint32_t T_display = 500; // Display refresh rate [ms]
+
+/*------------------------------------------------------------------------------
+  Frequency detector
+------------------------------------------------------------------------------*/
+
+const uint16_t N_AVG = 10;    // Number of up-flanks to average over
+const uint16_t TIMEOUT = 3000; // [ms] Timeout to stop detecting the frequency
+
+volatile bool isr_done = false;
+volatile uint8_t isr_counter = 0;
+volatile uint32_t T_upflanks = 0; // [us] Measured duration for N_AVG up-flanks
+
+double frequency = 0; // [Hz] Measured average frequency of light up-flanks
+double RPM = 0;       // [RPM] Measured RPM
+
+void isr_rising() {
+  /* Interrupt service routine for when an up-flank is detected on the input pin
+   */
+  static uint32_t micros_start = 0;
+
+  if (!isr_done) {
+    if (isr_counter == 0) {
+      micros_start = micros();
+    }
+    isr_counter++;
+    if (isr_counter > N_AVG) {
+      T_upflanks = micros() - micros_start;
+      isr_done = true;
+    }
+  }
+}
+
+bool measure_frequency() {
+  uint32_t t_0 = millis();
+  isr_counter = 0;
+  isr_done = false;
+  while ((!isr_done) && ((millis() - t_0) < TIMEOUT)) {}
+
+  if (isr_done) {
+    frequency = 1000000. * N_AVG / T_upflanks;
+    RPM = frequency / N_SLITS_ON_DISK * 60.;
+  } else {
+    frequency = 0;
+    RPM = 0;
+  }
+
+  return isr_done;
+}
+
+/*------------------------------------------------------------------------------
+  setup
+------------------------------------------------------------------------------*/
 
 void setup() {
   Serial.begin(9600);
 
-  // OLED buttons
-  pinMode(PIN_BUTTON_A, INPUT_PULLUP);
-  pinMode(PIN_BUTTON_B, INPUT_PULLUP);
-  pinMode(PIN_BUTTON_C, INPUT_PULLUP);
-
   // Tacho input
   pinMode(PIN_TACHO, INPUT_PULLDOWN);
+  attachInterrupt(digitalPinToInterrupt(PIN_TACHO), isr_rising, RISING);
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
-  display.display(); // Show Adafruit splashscreen from buffer
-  delay(1000);
-
-  display.clearDisplay(); // Clear the buffer
-  display.setTextSize(2);
+  display.clearDisplay();                    // Clear the buffer
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);
   display.display();
 }
 
+/*------------------------------------------------------------------------------
+  loop
+------------------------------------------------------------------------------*/
+
 void loop() {
-  bool fLight;
+  uint32_t now = millis();
+  static uint32_t tick = now;
+  static bool alive_blinker = true;
+  bool light;
 
-  fLight = digitalRead(PIN_TACHO);
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.print(fLight);
+  // light = digitalRead(PIN_TACHO);
+  light = digitalRead(PIN_BUTTON_C);
 
-  if (!digitalRead(PIN_BUTTON_A)) {
-    display.clearDisplay();
-    display.setCursor(0, 0);
-    display.print("A");
+  // Read the buttons
+  button_A.poll();
+  button_B.poll();
+  button_C.poll();
+  if (button_C.pushed()) {
+    alive_blinker = !alive_blinker;
   }
-  if (!digitalRead(PIN_BUTTON_B)) {
+
+  // Refresh screen
+  if (now - tick >= T_display) {
+    tick = now;
     display.clearDisplay();
+
+    // Draw RPM value
     display.setCursor(0, 0);
-    display.print("B");
+    display.setTextSize(3);
+    if (measure_frequency()) {
+      display.print(RPM, 3);
+      Serial.print(frequency, 2);
+      Serial.write('\t');
+      Serial.println(RPM, 3);
+    }
+
+    // Draw "RPM"
+    display.setTextSize(1);
+    // display.setCursor(110, 25); // Lower-right
+    display.setCursor(110, 0); // Upper-right
+    display.print("RPM");
+
+    // Draw alive blinker
+    alive_blinker = !alive_blinker;
+    if (alive_blinker) {
+      display.fillRect(0, 26, 6, 6, SSD1306_WHITE);
+    }
+
+    display.display();
   }
-  if (!digitalRead(PIN_BUTTON_C)) {
-    display.clearDisplay();
-    display.setCursor(0, 0);
-    display.print("C");
-  }
+
   delay(10);
-  yield();
-  display.display();
 }
